43ca7aabd935e8aa762e731a0f130b7bd7d39765
clementverc <clement.vercucque@orange.fr>
2019-03-19 21:01:32 +0100

    filter

--- front/src/HomePage/actions/index.js
+++ front/src/HomePage/actions/index.js
@@ -10,7 +10,7 @@ const newData = data => ({
 export const setSearchData = (filter) => {
   if (filter) {
     const el1 = document.querySelector('#SelectConstructor')
-    const Construteur = el1.options[el1.selectedIndex].text
+    const Constructeur = el1.options[el1.selectedIndex].text
     const el2 = document.querySelector('#SelectCategory')
     const Categorie = el2.options[el2.selectedIndex].text
     const el3 = document.querySelector('#SelectPermis')
@@ -18,19 +18,34 @@ export const setSearchData = (filter) => {
     const el4 = document.querySelector('#SelectGuidon')
     const Guidon = el4.options[el4.selectedIndex].text
 
-    const tabValue = [Construteur, Categorie, Permis, Guidon]
+    const tabValue = [Constructeur, Categorie, Permis, Guidon]
     const tabType = ['constructeur', 'categorie', 'permis', 'guidon']
     const tabStock = []
+    const flag = [false, false, false, false]
     moto.modeles.map((vehicule) => {
-      Object.keys(tabType).map((el, index) => {
-        if (vehicule[tabType[el[index]]] === tabValue[index]) {
+      Object.keys(tabType).map((el) => {
+        if (vehicule[tabType[el]] === tabValue[el]) {
+          flag[el] = true
+          console.log(flag)
+          console.log('tru')
+          console.log(vehicule.id)
+        } else {
+          console.log('false')
+          flag[el] = false
+        }
+        if (el === 2) {
+          return ''
+        }
+        if (flag[0] === true && flag[1] === true && flag[2] === true && flag[3] === true) {
           tabStock.push(vehicule)
         }
         return ''
       })
+      console.log('-----------')
       return ''
     })
     store.dispatch(newData(tabStock))
+    console.log(tabStock)
   } else {
     store.dispatch(newData(moto.modeles))
   }
--- front/src/HomePage/components/details.js
+++ front/src/HomePage/components/details.js
@@ -1,4 +1,4 @@
-import { Component } from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 class Details extends Component {
@@ -10,14 +10,17 @@ class Details extends Component {
 
     data.map((vehicule) => {
       if (detail === vehicule.id) {
-        dom = `Le modèle : ${vehicule.name} (${vehicule.cylindree} cc)
-          Crée par ${vehicule.constructeur} peut etre conduit avec un permis
-           ${vehicule.permis} puisqu'il a une puissance de ${vehicule.puissance} ch.
-          Ce modèle correspond à la categorie ${vehicule.categorie} et pèse ${vehicule.poid}.
-          Il est équipé d'un type de guidon : ${vehicule.guidon} et d'une 
-          distribution à ${vehicule.distribution}.
-          Il s'agit d'un modèle à refroidissement ${vehicule.refroidissement}.
-          Il mesure ${vehicule.longeur} cm de longueur et ${vehicule.largeur} cm de large.`
+        dom = (
+          <div className="container text-center">
+            <p>Le modèle : {vehicule.name} ({vehicule.cylindree} cc)</p>
+            <p>Crée par {vehicule.constructeur} peut etre conduit avec un permis</p>
+            <p>{vehicule.permis} car il a une puissance de {vehicule.puissance} ch.</p>
+            <p>Ce modèle correspond à la categorie {vehicule.categorie} et pèse {vehicule.poid}.</p>
+            <p> Il est équipé d un type de guidon : {vehicule.guidon} et d une distribution à {vehicule.distribution}.</p>
+            <p>Il s agit d un modèle à refroidissement {vehicule.refroidissement}.</p>
+            <p>Il mesure {vehicule.longeur} cm de longueur et {vehicule.largeur} cm de large.</p>
+          </div>
+        )
       }
       return ''
     })
